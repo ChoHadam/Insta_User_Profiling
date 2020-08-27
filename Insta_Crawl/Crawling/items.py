6a1d70a17bb864266efd1b6a37c01bccf9166fd1
Jisang <jisanghwang@naver.com>
2020-08-27 14:58:48 +0900

    insta crawling with scrapy

---
+++ Insta_Crawl/Crawling/items.py
@@ -0,0 +1,22 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstaCrawlingItem(scrapy.Item):
+    inner_id = scrapy.Field()
+    team_idx = 1
+    post_date = scrapy.Field()
+    view_count = scrapy.Field()
+    like_count = scrapy.Field()
+    crawling_time = scrapy.Field()
+    media_url = scrapy.Field()
+    content = scrapy.Field()
+    post_id = scrapy.Field()
+    url = scrapy.Field()
+    hashtag = scrapy.Field()
+    insta_id = scrapy.Field()
+    location = scrapy.Field()
